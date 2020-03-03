8de50a2bbc8c628cb5c1a506dd02fdc7911fda2a
LUIS AUGUSTO FERREIRA <blackbarth@outlook.com>
2020-03-03 19:04:00 -0300

    Projeto Criado

---
+++ src/test/java/br/com/luisaaugustoferreira/modelagemconceitual/ModelagemConceitualApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luisaaugustoferreira.modelagemconceitual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelagemConceitualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
